26ef4073373bfa484dfff5126649682efbdd16d4
mdgdeveloper <se.rgiomadrigal@gmail.com>
2024-08-21 21:11:31 +0200

    auth controller updated with the auth endpoints

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+// import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {
+    // This is where we inject the AuthService into the AuthController
+  }
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
+  @Post('register')
+  register(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  @Post('login')
+  login() {
+    // Login logic
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @Post('logut')
+  logout() {
+    // Logout logic
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
+  @Post('refresh')
+  refresh() {
+    // Refresh token logic
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('password-reset/request')
+  passwordResetRequest() {
+    // Password reset request logic
+  }
+
+  @Post('password-reset/reset')
+  passwordReset() {
+    // Password reset logic
   }
 }
